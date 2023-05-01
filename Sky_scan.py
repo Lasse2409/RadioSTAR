71a09bbb6ac396b0817659fb00c2b90c4fddd4f8
Lasse <lasse@LT_Vivobook.localdomain>
2023-05-01 16:15:49 +0200

    Updating Sky_scan.py

--- Sky_scan.py
+++ Sky_scan.py
@@ -9,31 +9,73 @@ import time
 
 
 
-N = 10
+def sky_box(N, AZ, EL):
+    sky_horizontal = np.zeros((N*N, 2))
 
+    az = np.linspace(AZ[0], AZ[1], N)
+    el = np.linspace(EL[0], EL[1], N)
+
+    for idx1, val1 in enumerate(az):
+        for idx2, val2 in enumerate(el):  
+            sky_horizontal[idx1*N + idx2, :] = np.array([val1,val2])
+    return sky_horizontal
+
+
+
+N = 10
+az_start = 220
+az_stop = 229
+
+el_start = 50
+el_stop = 59
 
 alt = 17.
 lat = 55.367511
 lon = 10.431889
 
 year = 2023
-month = 3
-day = 2
-hour = 19
-minute = 5
-second = 15
+month = 5
+day = 1
+hour = 16
+minute = 0
+second = 0
 
-sky = np.zeros((N*N, 2))
-
-az = np.linspace(0, 360, N)
-el = np.linspace(0, 90, N)
-
-for idx1, val1 in enumerate(az):
-    for idx2, val2 in enumerate(el):  
-        Sky[idx1*N + idx2, :] = np.array([val1,val2])
+sky_horizontal = sky_box(N, [az_start, az_stop],[el_start, el_stop])
+sky_galactic = np.transpose(coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, sky_horizontal[:,0], sky_horizontal[:,1], now = True))
 
 
-galactic = np.transpose(coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, Sky[:,0], Sky[:,1], now = True))
+sky_horizontal_measured = np.zeros((np.shape(sky_horizontal)))
+
+
+print(np.transpose(coordinates.Galactic_to_horizontal(year, month, day, hour, minute, second, lat, lon, alt, sky_galactic[0,0], sky_galactic[0,1], now = True)))
+
+for idx, val in enumerate(sky_horizontal): 
+    sky_horizontal_measured[idx,:] = np.transpose(coordinates.Galactic_to_horizontal(year, month, day, hour, minute, second, lat, lon, alt, sky_galactic[idx,0], sky_galactic[idx,1], now = True))
+    print(sky_horizontal_measured[idx,:])
+    time.sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#galactic = np.transpose(coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, sky_horizontal[:,0], sky_horizontal[:,1], now = True))
 
 #horizontal = np.transpose(coordinates.Galactic_to_horizontal(year, month, day, hour, minute, second, lat, lon, alt, galactic[:,0], galactic[:,1], now = True))
 
@@ -43,16 +85,16 @@ galactic = np.transpose(coordinates.Horizontal_to_galactic(year, month, day, hou
 #print(np.transpose(coordinates.Galactic_to_horizontal(year, month, day, hour, minute, second, lat, lon, alt, galactic[0], galactic[1], now = True)))
 #print(coordinates.Galactic_to_horizontal(year, month, day, hour, minute, second, lat, lon, alt, 20, 0, now = True))
 
-#print(coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, Sky[0,0], Sky[0,1], now = True))
-#print(coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, Sky[1,0], Sky[1,1], now = True))
-#print(coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, Sky[2,0], Sky[2,1], now = True))
+#print(coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, sky_horizontal[0,0], sky_horizontal[0,1], now = True))
+#print(coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, sky_horizontal[1,0], sky_horizontal[1,1], now = True))
+#print(coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, sky_horizontal[2,0], sky_horizontal[2,1], now = True))
 
 
 """
 galactic_l = coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, az, el, now = True)[0]
 galactic_b = coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, az, el, now = True)[1]
 
-horizontal_fixed_az = coordinates.Galactic_to_horizontal(year, month, day, hour, minute, second, lat, lon, alt, galactic_l, galactic_b, now = True)[0]
+horizontal_fixed_az = coordinates.Galactic_to_horizÆÆontal(year, month, day, hour, minute, second, lat, lon, alt, galactic_l, galactic_b, now = True)[0]
 horizontal_fixed_el = coordinates.Galactic_to_horizontal(year, month, day, hour, minute, second, lat, lon, alt, galactic_l, galactic_b, now = True)[1]
 
 
