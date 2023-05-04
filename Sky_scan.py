a98910b210d59add05c309c18f3e71ec8bf64f7e
Lasse <lasse@LT_Vivobook.localdomain>
2023-05-04 15:00:33 +0200

    blaa

--- Sky_scan.py
+++ Sky_scan.py
@@ -12,22 +12,8 @@ import os
 from src.Coordinate_transforms import coordinates
 from src.rotor import rotor
 
-def sky_box(N, AZ, EL):
-    sky_horizontal = np.zeros((N*N, 2))
+from src.utilities import utilities
 
-    az = np.linspace(AZ[0], AZ[1], N)
-    el = np.linspace(EL[0], EL[1], N)
-    el_reversed = np.flip(el,0)
-
-    for idx1 in range(len(az)):
-        for idx2 in range(len(el)):  
-            sky_horizontal[idx1*N + idx2, 0] = az[idx1]
-            if idx1%2 == 0:
-                sky_horizontal[idx1*N + idx2, 1] = el[idx2]
-            else:
-                sky_horizontal[idx1*N + idx2, 1] = el_reversed[idx2]
-
-    return sky_horizontal
 
 #azOffset = 222.8
 #elOffset = 1
@@ -53,7 +39,7 @@ hour = 16
 minute = 0
 second = 0
 
-sky_horizontal = sky_box(N, [az_start, az_stop],[el_start, el_stop])
+sky_horizontal = utilities.skyBox(N, [az_start, az_stop],[el_start, el_stop])
 sky_galactic = np.transpose(coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, sky_horizontal[:,0], sky_horizontal[:,1], now = True))
 
 sky_horizontal_measured = np.zeros((np.shape(sky_horizontal)))
