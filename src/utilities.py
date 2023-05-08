707f569a97e21bfa0dba5ef5d70c6ee08e586c60
Lasse <lasse@LT_Vivobook.localdomain>
2023-05-08 20:46:31 +0200

    data collection function added

--- src/utilities.py
+++ src/utilities.py
@@ -22,5 +22,39 @@ class utilities:
 
         return sky_horizontal
     
+        
+    def rtlSample(samples, sampleRate, centerFreq, gain, dataFileExtension):
+        sdr = RtlSdr()
+
+        sdr.sample_rate = sampleRate
+        sdr.center_freq = centerFreq
+        sdr.gain = gain
+
+        samples = sdr.read_samples(samples)
+        sdr.close()
+
+        f = open("data/single/singleData-" + str(dataFileExtension) + ".dat", "w")
+        
+        f.write("#Local time: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n")
+        f.write("#Latitude: " + str(lat))
+        f.write("#Longitude: " + str(lon))
+        f.write("#Altitude: " + str(alt))
+        f.write("#Az: " + str(measured_horizontal[idx, 0]) + "\n")
+        f.write("#El: " + str(measured_horizontal[idx, 1]) + "\n")
+        f.write("#Galactic latitude: " + str(measured_galactic[idx, 0]) + "\n")
+        f.write("#Galactic longitude: " + str(measured_galactic[idx, 1]) + "\n")
+        f.write("#RA: " + str(measured_equatorial[idx, 0]) + "\n")
+        f.write("#Dec: " + str(measured_equatorial[idx, 1]) + "\n")
+
+        ps, freqs = psd(samples, NFFT=256, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
+
+        for j in range(len(ps)):
+            f.write(str(ps[j]) + ", " + str(freqs[j]) + "\n")
+
+        f.close()
+
+        R.status()
+        print("Measuring data...")
+
     def psd():
         print('not finished function')
