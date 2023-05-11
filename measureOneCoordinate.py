effbfd50a6eb4a5e2484bd71d3b100c462f5cfe7
Lasse <lasse@LT_Vivobook.localdomain>
2023-05-11 12:11:02 +0200

    :)

--- measureOneCoordinate.py
+++ measureOneCoordinate.py
@@ -14,47 +14,23 @@ from src.rotor import rotor
 
 from src.utilities import utilities
 
-
-
-
-def rtlSample(samples, sampleRate, centerFreq, gain, filePathName, dataFileExtension, header):
-        sdr = RtlSdr()
-
-        sdr.sample_rate = sampleRate
-        sdr.center_freq = centerFreq
-        sdr.gain = gain
-
-        samples = sdr.read_samples(samples)
-        sdr.close()
-
-        f = open(filePathName + str(dataFileExtension) + ".dat", "w")
+def measurements(coordinate, target):
+    if coordinate == 0:
+        measured_horizontal = target
+        measured_galactic = np.transpose(coordinates.Horizontal_to_galactic(year, month, day, hour, minute, second, lat, lon, alt, target[0], target[1], now = True))
+        measured_equatorial = np.transpose(coordinates.Horizontal_to_equatorial(year, month, day, hour, minute, second, lat, lon, alt, target[0], target[1], now = True))
         
-        for idx in range(len(header)):
-            f.write(header[idx])
-
-        """
-        #makes header an input in function 
-        f.write("#Local time: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n")
-        f.write("#Latitude: " + str(lat))
-        f.write("#Longitude: " + str(lon))
-        f.write("#Altitude: " + str(alt))
-        f.write("#Az: " + str(measured_horizontal[idx, 0]) + "\n")
-        f.write("#El: " + str(measured_horizontal[idx, 1]) + "\n")
-        f.write("#Galactic latitude: " + str(measured_galactic[idx, 0]) + "\n")
-        f.write("#Galactic longitude: " + str(measured_galactic[idx, 1]) + "\n")
-        f.write("#RA: " + str(measured_equatorial[idx, 0]) + "\n")
-        f.write("#Dec: " + str(measured_equatorial[idx, 1]) + "\n")
-        """
-        
-        ps, freqs = psd(samples, NFFT=256, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
-
-        for j in range(len(ps)):
-            f.write(str(ps[j]) + ", " + str(freqs[j]) + "\n")
-
-        f.close()
-
-
-
+    elif coordinate == 1:
+        measured_horizontal = np.transpose(coordinates.Galactic_to_horizontal(year, month, day, hour, minute, second, lat, lon, alt, target[0], target[1], now = True))
+        measured_galactic = target
+        measured_equatorial = np.transpose(coordinates.Galactic_to_equatorial(target[0], target[1]))
+    elif coordinate == 2:
+        measured_horizontal = np.transpose(coordinates.Equatorial_to_horizontal(year, month, day, hour, minute, second, lat, lon, alt, target[0], target[1], now = True))
+        measured_galactic = np.transpose(coordinates.Equatorial_to_galactic(target[0], target[1]))
+        measured_equatorial = target
+    else:
+        print('wrong coordinate sys')
+        exit()
 
 
 
@@ -67,7 +43,13 @@ def rtlSample(samples, sampleRate, centerFreq, gain, filePathName, dataFileExten
 azOffset = 0
 elOffset  = 0
 
-target = [220,50]
+coordinate = 0 #0-> horizontal, 1-> galactic, 2-> equatorial
+target = np.array([[220,50]])
+
+measured_horizontal = np.zeros(np.shape(target))
+measured_equatorial = np.zeros(np.shape(target))
+measured_galactic = np.zeros(np.shape(target))
+
 
 alt = 17.
 lat = 55.367511
@@ -82,29 +64,35 @@ second = 0
 
 filePathName = "data/single/singleData-"
 
-header = []
-header.append("#Local time: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n")
-header.append("#Latitude: " + str(lat) + "\n")
-header.append("#Longitude: " + str(lon) + "\n")
-header.append("#Altitude: " + str(alt) + "\n")
-header.append("#Az: " + str(target[0]) + "\n")
-header.append("#El: " + str(target[1]) + "\n")
-#header.append("#Galactic latitude: " + str(measured_galactic[idx, 0]) + "\n")
-#header.append("#Galactic longitude: " + str(measured_galactic[idx, 1]) + "\n")
-#header.append("#RA: " + str(measured_equatorial[idx, 0]) + "\n")
-#header.append("#Dec: " + str(measured_equatorial[idx, 1]) + "\n")
-
-idx = 1
 
 R = rotor("192.168.1.104", 23)
 os.system("./../rtl-sdr-blog/build/src/rtl_biast -b 1")
 
 
 
+
 print("Going to: (" + str(target[0]) + ", " + str(target[1]) + ")")
 R.set(target[0] - azOffset, target[1] - elOffset)
 
-utilities.rtlSample(256*1024*4, 2.4e6, 1420e6, 49.6, filePathName, idx, header)
+
+measured_horizontal, measured_galactic, measured_equatorial = measurements(coordinate, target)
+
+header = []
+header.append("#Local time: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n")
+header.append("#Latitude: " + str(lat) + "\n")
+header.append("#Longitude: " + str(lon) + "\n")
+header.append("#Altitude: " + str(alt) + "\n")
+header.append("#Az: " + str(measured_horizontal[0]) + "\n")
+header.append("#El: " + str(measured_horizontal[1]) + "\n")
+header.append("#Galactic latitude: " + str(measured_galactic[0]) + "\n")
+header.append("#Galactic longitude: " + str(measured_galactic[1]) + "\n")
+header.append("#RA: " + str(measured_equatorial[0]) + "\n")
+header.append("#Dec: " + str(measured_equatorial[1]) + "\n")
+
+
+
+
+utilities.rtlSample(256*1024*4, 2.4e6, 1420e6, 49.6, filePathName, 1, header)
 
 R.status()
 print("Measuring data...")
