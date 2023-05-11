767b80f03e7a1825e933a9144a3dc9c0b23ffac6
Lasse <lasse@LT_Vivobook.localdomain>
2023-05-11 11:29:44 +0200

    :(

--- src/utilities.py
+++ src/utilities.py
@@ -24,7 +24,7 @@ class utilities:
         return sky_horizontal
     
         
-    def rtlSample(samples, sampleRate, centerFreq, gain, dataFileExtension, header):
+    def rtlSample(samples, sampleRate, centerFreq, gain, filePathName, dataFileExtension, header):
         sdr = RtlSdr()
 
         sdr.sample_rate = sampleRate
@@ -34,24 +34,11 @@ class utilities:
         samples = sdr.read_samples(samples)
         sdr.close()
 
-        f = open("data/single/singleData-" + str(dataFileExtension) + ".dat", "w")
+        f = open(filePathName + str(dataFileExtension) + ".dat", "w")
         
         for idx in range(len(header)):
             f.write(header[idx])
-
-        """
-        #makes header an input in function 
-        f.write("#Local time: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n")
-        f.write("#Latitude: " + str(lat))
-        f.write("#Longitude: " + str(lon))
-        f.write("#Altitude: " + str(alt))
-        f.write("#Az: " + str(measured_horizontal[idx, 0]) + "\n")
-        f.write("#El: " + str(measured_horizontal[idx, 1]) + "\n")
-        f.write("#Galactic latitude: " + str(measured_galactic[idx, 0]) + "\n")
-        f.write("#Galactic longitude: " + str(measured_galactic[idx, 1]) + "\n")
-        f.write("#RA: " + str(measured_equatorial[idx, 0]) + "\n")
-        f.write("#Dec: " + str(measured_equatorial[idx, 1]) + "\n")
-        """
+        
         ps, freqs = psd(samples, NFFT=256, Fs=sdr.sample_rate/1e6, Fc=sdr.center_freq/1e6)
 
         for j in range(len(ps)):
