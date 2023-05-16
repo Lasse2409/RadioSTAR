05294618cd611f6b335fa1c0713a63d12ae0ff9a
Lasse <lasse@LT_Vivobook.localdomain>
2023-05-16 15:33:56 +0200

    :)

--- stow.py
+++ stow.py
@@ -12,7 +12,14 @@ R = rotor("192.168.1.104", 23)
 ### Go to target this one we want to loop over and repeatedly update while data is being collected
 print("Going to stow position: (0, 90)")
 
-R.set(0 + azElOffset[0], 90 + azElOffset[1])
+target = [0, 90]
+
+if target[0] > 180:
+    setAz = -(360-target[0])
+else:
+    setAz = target[0]
+
+R.set(setAz + azElOffset[0], target[1] + azElOffset[1])
 
 print('In stow position: ')
 
