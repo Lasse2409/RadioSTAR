a806f391fadb49da0ff3385f6e6332d93ff4cfe7
Lasse <lasse@LT_Vivobook.localdomain>
2023-05-17 15:08:49 +0200

    :)

---
+++ workPosition.py
@@ -0,0 +1,24 @@
+# check howizontal coordinates of now, convert to galactic coordinates, and convert back to horizontal coordinates, check if above horizon. 
+import os
+from src.rotor import rotor
+
+
+
+### Initializeing seriel connection to rotor and turning on bias tee
+R = rotor("192.168.1.104", 23)
+
+
+### Go to target this one we want to loop over and repeatedly update while data is being collected
+print("Going to stow work position")
+
+
+
+R.set(266, 0)
+
+print('In stow position: ')
+
+R.status()
+os.system("./../rtl-sdr-blog/build/src/rtl_biast -b 0")
+
+print("Done!")
+
